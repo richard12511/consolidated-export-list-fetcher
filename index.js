3ee193f3f7122c2ccad5ad929cdd1c7246018a04
richard12511 <richardnew12511@gmail.com>
2017-03-10 09:47:01 -0600

    adds uploadToS3 function

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 var http = require('http')
+var AWS = require('aws-sdk')
+var s3 = new AWS.S3()
 
 function getJson(){
 
@@ -14,4 +16,24 @@ function getJson(){
 
 }
 
+function uploadToS3(){
+
+  var myBucket = 'export-control-prod';
+
+  var bucketName = 'export-control-prod'
+  var keyName = 'hello_world.txt'
+
+  s3.createBucket({Bucket: bucketName}, function() {
+    var params = {Bucket: bucketName, Key: keyName, Body: 'Hello World!'};
+    s3.putObject(params, function(err, data) {
+      if (err)
+        console.log(err)
+      else
+        console.log("Successfully uploaded data to " + bucketName + "/" + keyName);
+    });
+  });
+
+}
+
 getJson()
+uploadToS3()
