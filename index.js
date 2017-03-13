9c80aa8e8b9e39dfba4c821e3553bef86f9b6eea
richard12511 <richardnew12511@gmail.com>
2017-03-13 10:50:51 -0500

    adds first page of gov list to s3

--- index.js
+++ index.js
@@ -1,30 +1,16 @@
-var http = require('http')
+var https = require('https')
 var AWS = require('aws-sdk')
+var fetch = require('node-fetch')
+var co = require('co')
 var s3 = new AWS.S3()
+var baseUrl = "https://api.trade.gov/v2/consolidated_screening_list/search?api_key=hjuIpTBn7qlIO3_8mMRL_0gS&offset="
 
-function getJson(){
-
-  var request = http.get("http://date.jsontest.com/", function(response){
-
-    let rawData = ''
-    response.on('data', (chunk) => rawData += chunk)
-    response.on('end', () => {
-      var parsedData = JSON.parse(rawData)
-    })
-
-  })
-
-}
-
-function uploadToS3(){
-
-  var myBucket = 'export-control-prod';
+function uploadToS3(keyName, body){
 
   var bucketName = 'export-control-prod'
-  var keyName = 'hello_world.txt'
 
   s3.createBucket({Bucket: bucketName}, function() {
-    var params = {Bucket: bucketName, Key: keyName, Body: 'Hello World!'};
+    var params = {Bucket: bucketName, Key: keyName, Body: body};
     s3.putObject(params, function(err, data) {
       if (err)
         console.log(err)
@@ -32,8 +18,25 @@ function uploadToS3(){
         console.log("Successfully uploaded data to " + bucketName + "/" + keyName);
     });
   });
-
 }
 
-getJson()
-uploadToS3()
+co(function *() {
+  const firstPage = yield fetch(`${baseUrl}${0}`)
+  const firstJsonPage = yield firstPage.json()
+  const totalPages = firstJsonPage.total
+  let json = ''
+
+  for(let i = 0; i <= 9 ; i += 10){
+    let response = yield fetch(`${baseUrl}${0}`)
+    let jsonPage = yield response.json()
+    let results = jsonPage.results
+    let jsonResults = JSON.stringify(results)
+    jsonResults = jsonResults.replace(/]$/, "")
+    jsonResults = jsonResults.replace(/^\[/,"")
+    json += `${jsonResults},`
+  }
+
+  json = json.replace(/,\s*$/, "");
+  console.log(json)
+  uploadToS3('first-page.json', json)
+})
