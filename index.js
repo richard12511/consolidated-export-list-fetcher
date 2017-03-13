b30426357646b182879227a101a04835e883f39f
richard12511 <richardnew12511@gmail.com>
2017-03-13 11:39:10 -0500

    extracts getCurrentDate to separate function

--- index.js
+++ index.js
@@ -19,6 +19,22 @@ function uploadToS3(keyName, body){
   });
 }
 
+function getCurrentDate(){
+  let today = new Date();
+  let dd = today.getDate();
+  let mm = today.getMonth()+1; //January is 0!
+
+  let yyyy = today.getFullYear();
+  if(dd<10){
+    dd='0'+dd;
+  }
+  if(mm<10){
+    mm='0'+mm;
+  }
+
+  return `${mm}-${dd}-${yyyy}`
+}
+
 co(function *() {
   const firstPage = yield fetch(`${baseUrl}${0}`)
   const firstJsonPage = yield firstPage.json()
@@ -37,19 +53,6 @@ co(function *() {
   }
 
   json = json.replace(/,\s*$/, "");
-
-  let today = new Date();
-  let dd = today.getDate();
-  let mm = today.getMonth()+1; //January is 0!
-
-  let yyyy = today.getFullYear();
-  if(dd<10){
-    dd='0'+dd;
-  }
-  if(mm<10){
-    mm='0'+mm;
-  }
-
-  let currentDate = `${mm}-${dd}-${yyyy}`
+  let currentDate = getCurrentDate()
   uploadToS3(`${currentDate}.json`, json)
 })
